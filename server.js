c7e876f2b4ef1f53649e3c268d94a34f5e133d6e
lordneru <lordneru@gmail.com>
2018-01-22 03:51:33 +0100

    no message

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const exress = require('express');
 const app = express();
 
-app.use(express.static(_dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
