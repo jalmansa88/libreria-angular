e148c7b82d675ea69554dedc932e26726ff8a528
lordneru <lordneru@gmail.com>
2018-01-22 11:10:11 +0100

    no message

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const exress = require('express');
+const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/app'));
+app.listen(process.env.PORT || 3000);
+console.log("Server up on port 3000");
